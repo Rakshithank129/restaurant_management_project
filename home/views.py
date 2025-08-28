c48ddfd45210f30bf3af5f60b98b118431f71b41
RAKSHITHA N K <130299350+Rakshithank129@users.noreply.github.com>
2025-08-28 14:00:32 +0530

    39385

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def about(request):
+    """
+    Render the about page for the restaurant.
+    """
+    return render(request,"about.html")   
