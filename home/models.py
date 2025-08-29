21f7c1066b61a76f63f78a4283ae1742ac268f96
RAKSHITHA N K <130299350+Rakshithank129@users.noreply.github.com>
2025-08-29 21:58:00 +0530

    39384

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    """
+    Restaurant models stores basic information of restaurants like name and description.
+    """
+    name = models.CharField(max_length=100)
+    description= models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Restaurant"
+        verbose_name_plural = "Restaurants"
+
+        def __str__(self):
+            return self.name
+            
+
